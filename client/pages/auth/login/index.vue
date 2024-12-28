61a951ef78b6fe2646d08c39a0a4aab0152b684b
Petros <petros.rodinos@yahoo.com>
2024-12-28 13:44:43 +0200

    loginn signup validation

--- client/pages/auth/login/index.vue
+++ client/pages/auth/login/index.vue
@@ -1,24 +1,83 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { ElMessage, type FormRules, type FormInstance } from "element-plus";
+import { reactive, ref } from "vue";
 
-const formLabelAlign = reactive({
-  username: "",
+interface RuleForm {
+  email: string;
+  password: string;
+}
+
+const ruleFormRef = ref<FormInstance>();
+const loginForm = reactive<RuleForm>({
+  email: "",
   password: "",
 });
+
+const rules = reactive<FormRules<RuleForm>>({
+  email: [
+    {
+      required: true,
+      message: "Please input email address",
+      trigger: "blur",
+    },
+    {
+      type: "email",
+      message: "Please input correct email address",
+      trigger: ["blur", "change"],
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "Please input password",
+      trigger: "blur",
+    },
+    {
+      min: 6,
+      message: "Password length should be at least 6 characters",
+      trigger: "blur",
+    },
+  ],
+});
+
+const handleLogin = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+    } else {
+      ElMessage({
+        showClose: true,
+        message: "Please fill out all fields.",
+        type: "warning",
+      });
+    }
+  });
+};
 </script>
 
 <template>
   <div class="form-container">
     <h2>login</h2>
-    <el-form label-position="top" label-width="auto" style="max-width: 600px">
-      <el-form-item label="@username" label-position="top">
-        <el-input v-model="formLabelAlign.username" />
+    <el-form
+      :model="loginForm"
+      :rules="rules"
+      ref="ruleFormRef"
+      label-position="top"
+      label-width="auto"
+      style="max-width: 600px"
+    >
+      <el-form-item prop="email" label="Email">
+        <el-input v-model="loginForm.email" />
       </el-form-item>
-      <el-form-item label="password" label-position="top">
-        <el-input v-model="formLabelAlign.password" />
+
+      <el-form-item prop="password" label="password" label-position="top">
+        <el-input type="password" v-model="loginForm.password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="handleLogin(ruleFormRef)" type="success">login</el-button>
       </el-form-item>
     </el-form>
-    <p>dont have an account? <NuxtLink to="/auth/login">sign up</NuxtLink></p>
+    <p>don't have an account? <NuxtLink to="/auth/signup">sign up</NuxtLink></p>
   </div>
 </template>
 
--- client/pages/auth/signup/index.vue
+++ client/pages/auth/signup/index.vue
@@ -1,21 +1,96 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { ElMessage, type FormRules, type FormInstance } from "element-plus";
+import { reactive, ref } from "vue";
 
-const formLabelAlign = reactive({
+interface RuleForm {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const ruleFormRef = ref<FormInstance>();
+const signupForm = reactive<RuleForm>({
   username: "",
+  email: "",
   password: "",
 });
+
+const rules = reactive<FormRules<RuleForm>>({
+  username: [
+    {
+      required: true,
+      message: "Please input username",
+      trigger: "blur",
+    },
+    {
+      min: 4,
+      message: "Username length should be at least 4 characters",
+      trigger: "blur",
+    },
+  ],
+  email: [
+    {
+      required: true,
+      message: "Please input email address",
+      trigger: "blur",
+    },
+    {
+      type: "email",
+      message: "Please input correct email address",
+      trigger: ["blur", "change"],
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "Please input password",
+      trigger: "blur",
+    },
+    {
+      min: 6,
+      message: "Password length should be at least 6 characters",
+      trigger: "blur",
+    },
+  ],
+});
+
+const handleSignUp = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+    } else {
+      ElMessage({
+        showClose: true,
+        message: "Please fill out all fields.",
+        type: "warning",
+      });
+    }
+  });
+};
 </script>
 
 <template>
   <div class="form-container">
     <h2>sign up</h2>
-    <el-form label-position="top" label-width="auto" style="max-width: 600px">
-      <el-form-item label="@username" label-position="top">
-        <el-input v-model="formLabelAlign.username" />
+    <el-form
+      :model="signupForm"
+      :rules="rules"
+      ref="ruleFormRef"
+      label-position="top"
+      label-width="auto"
+      style="max-width: 600px"
+    >
+      <el-form-item prop="username" label="@username" label-position="top">
+        <el-input v-model="signupForm.username" />
       </el-form-item>
-      <el-form-item label="password" label-position="top">
-        <el-input v-model="formLabelAlign.password" />
+      <el-form-item prop="email" label="Email">
+        <el-input v-model="signupForm.email" />
+      </el-form-item>
+      <el-form-item prop="password" label="password" label-position="top">
+        <el-input type="password" v-model="signupForm.password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="handleSignUp(ruleFormRef)" type="success">sign up</el-button>
       </el-form-item>
     </el-form>
     <p>already have an account? <NuxtLink to="/auth/login">login</NuxtLink></p>
