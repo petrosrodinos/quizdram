228f87a4d63ca5f6a8f109ff1eb1b690a08388ba
Petros <petros.rodinos@yahoo.com>
2024-12-30 22:19:15 +0200

    new-home start

---
+++ client/components/create-quiz/index_old.vue
@@ -0,0 +1,86 @@
+<script lang="ts" setup>
+import { ref, defineAsyncComponent } from "vue";
+import { ArrowLeft } from "@element-plus/icons-vue";
+import type { NewQuiz } from "../../interfaces/quiz";
+import { navigateTo } from "nuxt/app";
+
+const step = ref(0);
+const selectedOption = ref("");
+const quizSettings = ref({});
+
+const steps: any = [
+  {
+    title: "choose a source",
+    component: "step1",
+  },
+  {
+    title: "create quiz",
+    component: "step2",
+  },
+  {
+    title: "finalize",
+    component: "step3",
+  },
+];
+
+const components: any = {
+  step1: defineAsyncComponent(() => import("./step1/index..vue")),
+  step2: defineAsyncComponent(() => import("./step2/index.vue")),
+  step3: defineAsyncComponent(() => import("./step3/index.vue")),
+};
+
+const handleOptionClick = (option: string) => {
+  selectedOption.value = option;
+  step.value++;
+};
+
+const handleCreateQuiz = (settings: NewQuiz) => {
+  quizSettings.value = {
+    ...quizSettings.value,
+    ...settings,
+  };
+  step.value++;
+};
+
+const handleGoBack = () => {
+  if (step.value > 0) step.value -= 1;
+};
+
+const handleFinalize = async () => {
+  await navigateTo("/user/quizes");
+};
+</script>
+
+<template>
+  <div class="create-quiz-container">
+    <h3>{{ steps[step].title }}</h3>
+    <component
+      :is="components[steps[step].component]"
+      :option="selectedOption"
+      @onOptionClick="handleOptionClick"
+      @onCreateQuiz="handleCreateQuiz"
+      :quizSettings="quizSettings"
+    />
+
+    <div class="step-buttons">
+      <el-button @click="handleGoBack" v-if="step > 0" type="danger" :icon="ArrowLeft"
+        >back</el-button
+      >
+      <el-button @click="handleFinalize" v-if="step == 2" type="success">finalize</el-button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.create-quiz-container {
+  padding: 1rem;
+  margin: 0 auto;
+  max-width: 800px;
+}
+
+.step-buttons {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+}
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="home-container">
     <CreateQuiz />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.home-container {
+  height: 92.5vh;
+  margin-top: 2px;
+  background: rgb(34, 193, 195);
+  background: linear-gradient(70deg, rgba(34, 193, 195, 1) 0%, rgba(253, 187, 45, 1) 61%);
+}
+</style>
